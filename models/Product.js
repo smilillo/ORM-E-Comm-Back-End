44a1efb5ecad8ae921362c29b1c8d1ee4c4be0e6
smilillo <sofiemilillo@gmail.com>
2023-04-25 18:52:14 -0400

    started filling out models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+      // validates value as decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // default value 10
+      // validates numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+      // reference category model id
+    }
   },
   {
     sequelize,
