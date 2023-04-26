e2803e5d14731ff2fc154aeae23297a75b7d91c1
smilillo <sofiemilillo@gmail.com>
2023-04-25 20:11:26 -0400

    started tag-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,9 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // need to include products too
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [{ model: Product, through: }]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -17,7 +19,7 @@ router.get('/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       // JOIN
-      include: 
+      include: [{ }]
     });
 
     if (!categoryData) {
